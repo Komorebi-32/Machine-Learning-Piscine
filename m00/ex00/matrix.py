1e734ff44e345d33438fa1e3c834f55f089cef19
komorebi-32 <karas.benjamin@gmail.com>
2026-07-27 14:57:45 +0200

    m00ex00: constructors and __str__ ok

--- m00/ex00/matrix.py
+++ m00/ex00/matrix.py
@@ -2,8 +2,38 @@
 
 class Matrix:
     def __init__(self, data):
-        self.data = data
-        self.shape = (len(data[0]), len(data[1]))
-    def __init__(self, shape):
-        self.shape = shape
-        self.data = [[[0] * shape[0]], [[0] * shape[1]]]
+        if type(data) is list:
+            self.data = data
+            self.shape = (len(data), len(data[0]))
+        elif type(data) is tuple:
+            self.shape = data
+            self.data = [[[0] * data[0]], [[0] * data[1]]]
+
+    def __str__(self):
+        if self.shape[0] == 1 or self.shape[1] == 1:
+            txt = "Vector("
+        else:
+            txt = "Matrix(" 
+        rows = []
+        for row in self.data:
+            row_str = ", ".join(map(str, row))
+            row_str = "[" + row_str + "]"
+            rows.append(row_str)
+        txt += ", \n".join(rows)
+        return txt
+
+# add : only matrices of same dimensions.
+# __add__
+# __radd__
+# # sub : only matrices of same dimensions.
+# __sub__
+# __rsub__
+# # div : only scalars.
+# __truediv__
+# __rtruediv__
+# # mul : scalars, vectors and matrices , can have errors with vectors and matrices,
+# # returns a Vector if we perform Matrix * Vector mutliplication.
+# __mul__
+# __rmul__
+# __str__
+# __repr__
---
+++ m00/ex00/test.py
@@ -0,0 +1,7 @@
+from matrix import Matrix
+
+mat_zero = Matrix((3, 3))
+mat = Matrix([[1, 2, 3], [4, 5, 6], [32, 8, 9]])
+
+print(mat_zero)
+print(mat)
