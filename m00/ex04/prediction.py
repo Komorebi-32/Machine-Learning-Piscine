caa866211689e083629ff5b9fb6f5ae1f42cdf06
komorebi-32 <karas.benjamin@gmail.com>
2026-07-28 12:05:02 +0200

    m00ex04 error checks ok

---
+++ m00/ex04/prediction.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def predict_(x, theta):
+    """Computes the vector of prediction y_hat from two non-empty numpy.array.
+    Args:
+    x: has to be an numpy.array, a one-dimensional array of size m.
+    theta: has to be an numpy.array, a two-dimensional array of shape 2 * 1.
+    Returns:
+    y_hat as a numpy.array, a two-dimensional array of shape m * 1.
+    None if x and/or theta are not numpy.array.
+    None if x or theta are empty numpy.array.
+    None if x or theta dimensions are not appropriate.
+    Raises:
+    This function should not raise any Exceptions.
+    """
+    if not isinstance(x, np.ndarray) or not isinstance(theta, np.ndarray):
+        print("Error: x and/or theta should be of type numpy.ndarray")
+        return None
+    if x.size == 0 or theta.size == 0:
+        print("Error: x and/or theta should not be empty")
+        return None
+    if x.ndim != 1 or theta.shape != (2, 1):
+        print("Error: x or theta dimensions are not appropriate.")
+        return None
